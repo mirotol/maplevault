badfd003aa45677249271a96683ce73083539d63
mirotol <mirotolkkila@gmail.com>
2026-03-16 21:12:11 +0200

    Add homepage gif

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,9 @@
-import { Database } from "lucide-react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import FallingEffect from "../components/FallingEffect";
 
 const HomePage = () => {
+  const [loaded, setLoaded] = useState(false);
+
   useEffect(() => {
     document.title = "Home | MapleVault";
   }, []);
@@ -10,15 +11,27 @@ const HomePage = () => {
   return (
     <div className="flex flex-col items-center justify-center py-24 text-center">
       <FallingEffect image="/effects/maple_leaf.png" count={7} />
-      <div className="w-24 h-24 mb-8 rounded-3xl bg-(--color-accent-bg) flex items-center justify-center text-(--color-accent)">
-        <Database className="w-12 h-12" />
-      </div>
-      <h1 className="font-heading text-5xl font-bold mb-6">
-        Welcome to MapleVault
-      </h1>
-      <p className="text-2xl opacity-70 max-w-2xl leading-relaxed">
-        MapleStory database for mobs and items. Based on GMS v.83
-      </p>
+
+      <img
+        src="/effects/library.gif"
+        alt="MapleVault Library"
+        onLoad={() => setLoaded(true)}
+        className={`w-86 mb-10 mt-4 pointer-events-none select-none drop-shadow-xl ${
+          loaded ? "opacity-100" : "opacity-0"
+        }`}
+      />
+
+      {loaded && (
+        <>
+          <h1 className="font-heading text-6xl font-bold text-white mb-4 drop-shadow-[0_3px_0_rgba(0,0,0,0.3)]">
+            Welcome to MapleVault
+          </h1>
+
+          <p className="text-2xl text-white/80 drop-shadow-sm max-w-2xl leading-relaxed">
+            MapleStory database for mobs and items. Based on GMS v.83
+          </p>
+        </>
+      )}
     </div>
   );
 };
